7bca23de5d2950b36aadd1370ef21fa817561ace
tony2020-god <62175480+tony2020-god@users.noreply.github.com>
2021-07-03 15:55:20 +0800

    完成

--- 2D/Assets/程式碼/AnimationControl.cs
+++ 2D/Assets/程式碼/AnimationControl.cs
@@ -1,8 +1,17 @@
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;//引用系統集合、管理API(協同程式:非同步處理)
 
 public class AnimationControl : MonoBehaviour
 {
-   
+
+    public void Start()
+    {
+        if (gameObject.layer == LayerMask.NameToLayer("隕石爆炸"))
+        {
+            StartCoroutine(DESOB());
+        }
+    }
     void Update()
     {
         if (LVsave.magiciandead == true && gameObject.layer == LayerMask.NameToLayer("隕石"))
@@ -18,5 +27,10 @@ public class AnimationControl : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+    public IEnumerator DESOB()
+    {
+       yield return new WaitForSeconds(5);
+        Destroy(gameObject);
     }
 }
